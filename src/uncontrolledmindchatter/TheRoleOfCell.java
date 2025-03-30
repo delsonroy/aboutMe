b5d3dfbd1998c30d9d845ab88181b1b0ef998d1e
delsonroy <delson.geo@gmail.com>
2025-03-29 20:18:24 -0400

    new

--- src/uncontrolledmindchatter/TheRoleOfCell.java
+++ src/uncontrolledmindchatter/TheRoleOfCell.java
@@ -12,6 +12,15 @@ public class TheRoleOfCell {
 		
 		
 	}
+	public void Environmental_cues2()
+	{
+		System.out.println("e.g., stress, noise, or danger .");
+		System.out.println("If you experience stress, your cells may increase cortisol production, "
+				+ "activating genes that heighten fear or negative thinking.");
+		
+		
+		
+	}
 	public void Emotional_Trigger()
 	{
 		System.out.println("Emotional triggers that activate certain genes. and then activates the mind voice");
