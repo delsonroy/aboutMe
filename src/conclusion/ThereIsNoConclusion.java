8e8354170d33a4958965d7507824327cff59ec41
delsonroy <delson.geo@gmail.com>
2025-03-30 10:21:27 -0400

    This is the latest ocommi

---
+++ src/conclusion/ThereIsNoConclusion.java
@@ -0,0 +1,12 @@
+package conclusion;
+
+public class ThereIsNoConclusion {
+
+	public static void main(String[] args) {
+
+		System.out.println("There is not end in the human suffering ");
+		System.out.println("There is not end in the human suffering ");
+		System.out.println("I know there is a place to live k c");
+	}
+
+}
