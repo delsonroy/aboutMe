a8ef617bfc896c4b0ac3a8028f48387481df7bb7
delsonroy <delson.geo@gmail.com>
2025-04-03 20:36:29 -0400

    Latest_commit

---
+++ src/newlearnings/Inner_Purpose.java
@@ -0,0 +1,17 @@
+package newlearnings;
+
+public class Inner_Purpose {
+
+	public void Whats_your_inner_purpose()
+	{
+		
+		System.out.println("Deep down there is the essence of Gd");
+		
+	}
+	public void Whats_your_inner_purpose2()
+	{
+		
+		
+	}
+
+}
