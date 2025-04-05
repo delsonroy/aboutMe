08b23d32afdf7f6fdb1960b6a37f17b53881eecd
delsonroy <delson.geo@gmail.com>
2025-04-05 19:38:04 -0400

    latest commit

--- src/itsnotme/ItsNotme.java
+++ src/itsnotme/ItsNotme.java
@@ -5,7 +5,16 @@ public class ItsNotme {
 	public static void main(String[] args) {
 		{
 			
-			System.out.println("Hi How are you...");
+			/*
+			 * I am not Delson, I am the Holy spirit
+			 * or essense of this unversal life manifested in the body
+			 * 
+			 * 
+			 * 
+			 * 
+			 * 
+			 * 
+			 */
 			
 			
 		}
