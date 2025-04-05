ae63ea088cbbf023c17792359852d8340783b89f
delsonroy <delson.geo@gmail.com>
2025-04-05 19:42:40 -0400

    Latest Commit

--- src/itsnotme/ItsNotme.java
+++ src/itsnotme/ItsNotme.java
@@ -8,6 +8,8 @@ public class ItsNotme {
 			/*
 			 * I am not Delson, I am the Holy spirit
 			 * or essense of this unversal life manifested in the body
+			 * Whatever I am doing can it honor the jesus christ? if not
+			 * How can I honor the jesus christ 
 			 * 
 			 * 
 			 * 
