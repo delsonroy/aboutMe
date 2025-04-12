37a6b34e12fd35aa7490e297f5e2309949c95bbd
delsonroy <delson.geo@gmail.com>
2025-04-12 18:56:58 -0400

    Added comments

--- src/notesfromthebookthatlittlevoiceinyourhead/Saturday1204.java
+++ src/notesfromthebookthatlittlevoiceinyourhead/Saturday1204.java
@@ -6,6 +6,16 @@ public class Saturday1204 {
 	public void notes()
 	{
 		System.out.println("These are some points from that book");
+		/*
+		 * The output processed by our brain is the input you given to the Brain
+		 * your brain can help you to learn new programs
+		 * Learn new language
+		 * Learn about new things, it can help you crate anything you want yo
+		 * The thing is that you should know what you want 
+		 * If you can concentrate and navigate one small things and study about
+		 * it vastly then that will help you to monotize
+		 * 
+		 */
 	}
 	
 	
