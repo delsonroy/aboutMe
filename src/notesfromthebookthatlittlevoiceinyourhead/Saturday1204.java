f825996fccc23e3018f6720b124545b0a3fd47d8
delsonroy <delson.geo@gmail.com>
2025-04-12 18:49:14 -0400

    Adding new package

---
+++ src/notesfromthebookthatlittlevoiceinyourhead/Saturday1204.java
@@ -0,0 +1,13 @@
+package notesfromthebookthatlittlevoiceinyourhead;
+
+public class Saturday1204 {
+	
+	
+	public void notes()
+	{
+		System.out.println("These are some points from that book");
+	}
+	
+	
+
+}
